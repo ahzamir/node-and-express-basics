22a39406f52267c4c80bc1d6a896a76fc586957c
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2023-04-25 15:43:44 +0430

    created a root route and render a json format on it and created a view file with ejs library

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+    console.log("Hello")
+    res.render('index.js')
+})
+
 app.listen(3000, () => console.log('Server running on port 3000'))
