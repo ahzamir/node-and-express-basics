cb59281e7515423441dad2514a2a2e987727af50
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2023-04-25 21:11:32 +0430

    practiced with middleware by creating a logger function and call it in different routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+router.use(logger)
 
 router.get('/', (req, res) => {
     res.send('Users List')
@@ -32,5 +33,10 @@ router.param('id', (req, res, next, id) => {
     req.user = users[id]
     next()
 })
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,17 +2,11 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
-app.use(logger)
 
 app.get('/', (req, res) => {
-    console.log("Hello")
     res.render('index', { name: 'Zamir' })
 })
 
-function logger(req, res, next) {
-    console.log(req.originalUrl)
-    next()
-}
 const userRouter = require('./routes/users')
 
 app.use('/users', userRouter)
