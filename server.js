532ec7b5d2d243749bb4ec08608a5cef68364ae1
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2023-04-25 19:55:47 +0430

    practicing rendering HTML on server and passing some params to HTML filr usig ejs and express

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     console.log("Hello")
-    res.render('index.js')
+    res.render('index', { name: 'Zamir' })
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
